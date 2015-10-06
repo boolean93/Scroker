b6a7dd7375a803cc842e36a222a7291b9ef93b71
boolean93 <boolean93@163.com>
2015-10-06 10:29:34 +0800

    clean

--- Scroker/SPMainViewController.m
+++ Scroker/SPMainViewController.m
@@ -8,12 +8,10 @@
 
 #import "SPMainViewController.h"
 #import "SPMainViewCell.h"
-#import "SPMainPokerView.h"
 #import "SPCollectionViewLayout.h"
 #import "SPPokerDetailController.h"
 
 @interface SPMainViewController ()
-@property (strong, nonatomic) SPMainPokerView *pokerView;
 @property (assign, nonatomic) NSString *pokerNumber;
 @end
 
@@ -25,8 +23,6 @@ static CGFloat kItemHeight = 150.f;
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    self.pokerView = [[NSBundle mainBundle] loadNibNamed:@"SPMainPokerView" owner:self options:nil][0];
-    [self.view addSubview:self.pokerView];
     SPCollectionViewLayout *layout = (SPCollectionViewLayout *)self.collectionViewLayout;
     layout.itemSize = CGSizeMake(kItemHeight * 0.618, kItemHeight);
     layout.headerReferenceSize = CGSizeMake(0, 30);
@@ -89,9 +85,6 @@ static CGFloat kItemHeight = 150.f;
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
-//    SPMainViewCell *cell = (SPMainViewCell *)[self collectionView:collectionView cellForItemAtIndexPath:indexPath];
-//    self.pokerView.frame = cell.frame;
-//    self.pokerView.number.text = cell.number.text;
 }
 /*
 // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
